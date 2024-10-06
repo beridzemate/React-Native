6745eec681bb2d5f6587c746ca0db169b42bbda8
Qotnis ajafsandali <beridzem200@gmail.com>
2024-10-06 05:45:14 -0400

    ...

--- app/main/src/lib/downloadGroupGraph.js
+++ app/main/src/lib/downloadGroupGraph.js
@@ -17,28 +17,28 @@ export default async function downloadGroupGraph(groupId) {
   graph.forEachNode(node => {
     node.data.l = node.data.l.split(',').map(x => +x);
   })
-  // let nodeFetch = fetch(`${config.graphsEndpoint}/${groupId}.nodes.json`).then(r => r.json()).then(nodePositions => {
-  //   let nodeId = 0;
-  //   for (let i = 0; i < nodePositions.length; i += 2) {
-  //     nodeId += 1;
-  //     graph.addNode(nodeId, [nodePositions[i], nodePositions[i + 1]]);
-  //   }
-  // });
-  // let linkFetch = fetch(`${config.graphsEndpoint}/${groupId}.links.bin`).then(r => r.arrayBuffer()).then(buffer => {
-  //   // buffer is int32 array:
-  //   let view = new Int32Array(buffer);
-  //   let lastFrom = 0;
-  //   for (let i = 0; i < view.length; i++) {
-  //     let v = view[i];
-  //     if (v < 0) {
-  //       // new source node
-  //       lastFrom = -v;
-  //       continue;
-  //     }
-  //     graph.addLink(lastFrom, v);
-  //   }
-  // });
-  // await Promise.all([nodeFetch, linkFetch]);
+  let nodeFetch = fetch(`${config.graphsEndpoint}/${groupId}.nodes.json`).then(r => r.json()).then(nodePositions => {
+    let nodeId = 0;
+    for (let i = 0; i < nodePositions.length; i += 2) {
+      nodeId += 1;
+      graph.addNode(nodeId, [nodePositions[i], nodePositions[i + 1]]);
+    }
+  });
+  let linkFetch = fetch(`${config.graphsEndpoint}/${groupId}.links.bin`).then(r => r.arrayBuffer()).then(buffer => {
+    // buffer is int32 array:
+    let view = new Int32Array(buffer);
+    let lastFrom = 0;
+    for (let i = 0; i < view.length; i++) {
+      let v = view[i];
+      if (v < 0) {
+        // new source node
+        lastFrom = -v;
+        continue;
+      }
+      graph.addLink(lastFrom, v);
+    }
+  });
+  await Promise.all([nodeFetch, linkFetch]);
   graphsCache.set(groupId, graph);
   return graph;
 }
